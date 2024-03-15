af75df25f1c73b1a528ea71285e7d2defe8cb146
avabellaw <70448139+avabellaw@users.noreply.github.com>
2024-03-15 16:32:36 +0000

    Correct structure of project

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-if29lew&29cns2f$w%@=eh8r8@8j=g6s#zck#wzu$m=npu3m0('
+SECRET_KEY = 'django-insecure-7!e+g68q2o4#yrd3!4c^adnla-ilrqkk5uj#_xxd!wnj#9@qhr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
