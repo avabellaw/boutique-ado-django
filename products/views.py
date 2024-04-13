6656fb21c77f84c797b61b2f6c4fabdefc73f94e
avabellaw <70448139+avabellaw@users.noreply.github.com>
2024-04-13 20:43:47 +0100

    Add search queries and category filtering

--- products/views.py
+++ products/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Product
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
+from .models import Product, Category
 
 # Create your views here.
 
@@ -8,8 +10,29 @@ def all_products(request):
     """View to show all products, including sort and search"""
 
     products = Product.objects.all()
+    query = None
+    categories = None
+
+    if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+            
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "Search query is blank!")
+                return redirect(reverse('products'))
+
+            queries = Q(name__icontains=query) | Q(
+                description__icontains=query)
+            products = products.filter(queries)
+
     context = {
         'products': products,
+        'search_term': query,
+        'current_categorys': categories,
     }
 
     return render(request, 'products/products.html', context)
