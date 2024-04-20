95b5431dff0337c9540e7cd8773a9dd70ef35fd5
avabellaw <70448139+avabellaw@users.noreply.github.com>
2024-04-20 22:33:21 +0100

    Add logic for checkout and payment success

--- checkout/models.py
+++ checkout/models.py
@@ -37,7 +37,8 @@ class Order(models.Model):
         """
         Update grand total for each line item
         """
-        self.order_total = self.lineitems.aggregate(Sum('lineitems_total'))['lineitem_total__sum']
+        self.order_total = self.lineitems.aggregate(
+            Sum('lineitem_total'))['lineitem_total__sum'] or 0
 
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
             self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE
