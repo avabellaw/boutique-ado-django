64d22ecf6f5ab3b478023eb0b151542c97caefd8
avabellaw <70448139+avabellaw@users.noreply.github.com>
2024-04-25 23:22:06 +0100

    Set debug to True only if DEVELOPMENT in environ

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = "DEVELOPMENT" in os.environ
 
 ALLOWED_HOSTS = ['boutique-ado-django-ava-3965bd992e78.herokuapp.com']
 
