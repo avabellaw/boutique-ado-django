7b67fb851e72a45d9953ad8dac31a038f4278e4c
avabellaw <70448139+avabellaw@users.noreply.github.com>
2024-04-25 17:40:50 +0100

    Update webhook handler to add profile

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from .models import Order, OrderLineItem
 from products.models import Product
+from profiles.models import UserProfile
 import stripe
 import json
 import time
@@ -43,10 +44,25 @@ class StripeWebhookHandler:
             if value == "":
                 shipping_details['address'][field] = None
 
+        # Update profile info if save_info was checked
+        profile = None
+        username = intent['metadata']['username']
+        if username != 'anonymousUser':
+            profile = UserProfile.objects.get(user__username=username)
+            if save_info:
+                profile.default_phone_number__iexact = shipping_details['phone']
+                profile.default_country__iexact = shipping_details['address']['country']
+                profile.default_postcode__iexact = shipping_details['address']['postal_code']
+                profile.default_town_or_city__iexact = shipping_details['address']['city']
+                profile.default_street_address1__iexact = shipping_details['address']['line1']
+                profile.default_street_address2__iexact = shipping_details['address']['line2']
+                profile.default_county__iexact = shipping_details['address']['state']
+                profile.save()
+
         order_exists = False
 
         attempt = 1
-        
+
         while attempt <= 5:
             try:
 
@@ -79,17 +95,18 @@ class StripeWebhookHandler:
         else:
             try:
                 order = Order.objects.create(
-                        full_name=shipping_details['name'],
-                        email=billing_details['email'],
-                        phone_number=shipping_details['phone'],
-                        country=shipping_details['address']['country'],
-                        postcode=shipping_details['address']['postal_code'],
-                        town_or_city=shipping_details['address']['city'],
-                        street_address1=shipping_details['address']['line1'],
-                        street_address2=shipping_details['address']['line2'],
-                        county=shipping_details['address']['state'],
-                        original_bag=bag,
-                        stripe_pid=pid,
+                    full_name=shipping_details['name'],
+                    user_profile=profile,
+                    email=billing_details['email'],
+                    phone_number=shipping_details['phone'],
+                    country=shipping_details['address']['country'],
+                    postcode=shipping_details['address']['postal_code'],
+                    town_or_city=shipping_details['address']['city'],
+                    street_address1=shipping_details['address']['line1'],
+                    street_address2=shipping_details['address']['line2'],
+                    county=shipping_details['address']['state'],
+                    original_bag=bag,
+                    stripe_pid=pid,
                 )
 
                 for item_id, item_data in json.loads(bag).items():
