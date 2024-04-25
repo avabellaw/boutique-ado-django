bd5d3fa442f42bc08a06084fca5a130d58ad77c9
avabellaw <70448139+avabellaw@users.noreply.github.com>
2024-04-26 00:17:03 +0100

    Add configuration for the S3 bucket

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'profiles',
 
     'crispy_forms',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -186,3 +187,21 @@ STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET', '')
 
 DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
+
+# Heroku configuration for s3 bucket
+if 'USE_AWS' in os.environ:
+    AWS_STORAGE_BUCKET_NAME = 'boutique-ado-django-ava'
+    AWS_S3_REGION_NAME = 'us-east-1'
+    AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+    AWS_ACCESS_KEY = os.environ.get("AWS_ACCESS_KEY")
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_LOCATION = 'static'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    MEDIAFILES_LOCATION = 'media'
+    
+    # override static and media URLS in production
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}'
+    
